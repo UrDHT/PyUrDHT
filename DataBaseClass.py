21486967bacc5964a90c375e78a4ff4d82386e6b
Brendan Benshoof <bbenshoof1@student.cs.gsu.edu>
2015-06-29 10:52:29 -0400

    Added helper method of backups

--- DataBaseClass.py
+++ DataBaseClass.py
@@ -36,3 +36,7 @@ class DataBase(object):
             return [x for x in filter(lambda x: x[0]>t,self.streams[id])]
         except:
             return []
+
+    def recordList(self):
+        return self.records.keys()
+    
