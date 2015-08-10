df65087abd2a40b24787d2d6ab4174cf56ae6ae2
Andrew Rosen <rosen@cs.gsu.edu>
2015-08-10 15:08:17 -0400

    If you forget the trailing / in your bindAddr, we'll add it back for you, no extra charge

--- main.py
+++ main.py
@@ -63,6 +63,9 @@ if __name__=="__main__":
         random_peer = random.choice(peerPool)
         pubip = net.getIP(random_peer)
         path = "http://%s:%d/" % (pubip, port)
+    elif path[-1] != "/":
+        path += "/"
+
 
     hashid = genHash(path,0x12)
 
