a226d8a7d035f3cf8ed25c5330ba8f73dc08b759
abrosen <rosen@cs.gsu.edu>
2015-08-28 15:27:21 -0400

    formatted

--- services/chord/chordnetwork.py
+++ services/chord/chordnetwork.py
@@ -2,9 +2,9 @@
 This file describes the Networking class and associated helper threads/objects
 see: https://github.com/UrDHT/DevelopmentPlan/blob/master/Network.md
 """
-import chordserver as server
+import server
 import myrequests as requests
-from chordlogic import PeerInfo
+import util
 
 
 class DialFailed(Exception):
@@ -12,29 +12,29 @@ class DialFailed(Exception):
 
 
 class Networking(object):
+
     def __init__(self, ip, port):
         self.ip = ip
         self.port = port
         self.handlers = {}
         self.netHandlers = {}
 
-
-    def setup(self,logic,data):
+    def setup(self, logic, data):
         """
         Lets the server know which LogicClass and DataClass to use
         """
 
-        self.serverThread = server.getThread(self.ip,self.port)
+        self.serverThread = server.getThread(self.ip, self.port)
         self.serverThread.start()
         self.handlers["UrDHT"] = logic
         self.netHandlers["UrDHT"] = None
-        server.setLinks(self.handlers,self.netHandlers,data)
+        server.setLinks(self.handlers, self.netHandlers, data)
 
-    def addHandler(self,key,logic,netHandler):
+    def addHandler(self, key, logic, netHandler):
         self.handlers[key] = logic
         self.netHandlers[key] = netHandler
 
-    def seek(self,service, remote, id):
+    def seek(self, service, remote, id):
         """
         This function remotely calls seek on the remote node,
         asking it to run LogicComponent.seek() on id
@@ -44,22 +44,22 @@ class Networking(object):
         try:
             r = requests.get(path)
             results = r.json()
-            val = PeerInfo(results["id"], results["addr"], results["loc"])
+            val = util.PeerInfo(results["id"], results["addr"], results["loc"])
         except Exception:
             raise DialFailed()
         return val
 
-    def getPeers(self,service,remote):
+    def getPeers(self, service, remote):
         result = []
         try:
-            r = requests.get(remote.addr+service+"/peer/getPeers/")
-            #print(r.text)
-            #print(r.text)
-            #print(r.text)
+            r = requests.get(remote.addr + service + "/peer/getPeers/")
+            # print(r.text)
+            # print(r.text)
+            # print(r.text)
             if len(r.json()) == 0:
                 return []
             for p in r.json():
-                newPeer = util.PeerInfo(p["id"],p["addr"],p["loc"])
+                newPeer = util.PeerInfo(p["id"], p["addr"], p["loc"])
                 result.append(newPeer)
         except Exception:
             raise DialFailed()
@@ -68,58 +68,57 @@ class Networking(object):
     def getSuccessors(self, service, remote):
         result = []
         try:
-            r = requests.get(remote.addr+service+"/peer/getSuccessors/")
+            r = requests.get(remote.addr + service + "/peer/getSuccessors/")
             if len(r.json()) == 0:
                 return []
             for p in r.json():
-                newPeer = util.PeerInfo(p["id"],p["addr"],p["loc"])
+                newPeer = util.PeerInfo(p["id"], p["addr"], p["loc"])
                 result.append(newPeer)
         except Exception:
             raise DialFailed()
         return result
 
     def getPredecessor(self, service, remote):
-        result =  []
+        result = []
         try:
-            r = requests.get(remote.addr+service+"/peer/getPredecessor/")
+            r = requests.get(remote.addr + service + "/peer/getPredecessor/")
             if len(r.json()) == 0:
                 return []
             for p in r.json():
-                newPeer = util.PeerInfo(p["id"],p["addr"],p["loc"])
+                newPeer = util.PeerInfo(p["id"], p["addr"], p["loc"])
                 result.append(newPeer)
         except Exception:
             raise DialFailed()
         return result
 
-    def notify(self,service,remote,origin):
-        #print("SENDING NOTIFY",remote,origin)
+    def notify(self, service, remote, origin):
+        # print("SENDING NOTIFY",remote,origin)
         try:
-            r = requests.post(remote.addr+service+"/peer/notify", data=str(origin))
+            r = requests.post(remote.addr + service + "/peer/notify", data=str(origin))
             return r.status_code == requests.codes.ok
         except:
             return False
 
-    def store(self,service,remote,id,data):
-        #print("SENDING NOTIFY",remote,origin)
+    def store(self, service, remote, id, data):
+        # print("SENDING NOTIFY",remote,origin)
         try:
-            r = requests.post(remote.addr+service+"/client/store/"+id, data=data)
+            r = requests.post(remote.addr + service + "/client/store/" + id, data=data)
             return r.status_code == requests.codes.ok
         except:
             return False
 
-
-    def getIP(self,service,remote):
+    def getIP(self, service, remote):
         ip = None
         try:
-            r = requests.get(remote.addr+service+"/peer/getmyIP")
+            r = requests.get(remote.addr + service + "/peer/getmyIP")
             ip = r.text
         except:
             raise DialFailed()
         return ip
 
-    def ping(self,service,remote):
+    def ping(self, service, remote):
         try:
-            r = requests.get(remote.addr+service+"/peer/ping")
+            r = requests.get(remote.addr + service + "/peer/ping")
 
             return r.status_code == requests.codes.ok
         except:
