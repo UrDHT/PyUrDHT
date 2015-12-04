d5503c9d6be4a43049df250c3fbd813184c8370c
abrosen <rosen@cs.gsu.edu>
2015-12-04 17:52:33 -0500

    Okay now it's fleshing out and I solved my import problem with black magic

--- services/mapreduce/ChunkStorage.py
+++ services/mapreduce/ChunkStorage.py
@@ -3,11 +3,14 @@
 This file describes the Database class and associated helper threads/objects
 see: https://github.com/UrDHT/DevelopmentPlan/blob/master/Database.md
 
-
-
 """
 
-import time
+import time, sys, os.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname("../../vendored/"), os.path.pardir )))
+from vendored import pymultihash as MultiHash
+
+DEFAULT_BLOCK_SIZE = 1024 * 8 # bytes
+MAX_BLOCK_SIZE = float("inf")
 
 
 class DataBase(object):
@@ -54,5 +57,49 @@ class DataBase(object):
 class KeyFile(object):
 
     def __init__(self, primaryKey):
-        self.key =  primaryKey
+        self.key = primaryKey
+        self.chunkKeys = []
+
+
+class Chunk(object):
+
+    def __init__(self, chunkKey, contents):
+        self.key = chunkKey
+        self.contents = contents
+
+
+
+
+def makeChunks(filename, chunkGenerator):
+    primaryKey =  MultiHash.genHash(filename , 0x12)
+    keyfile = KeyFile(primaryKey)
+    chunks = []
+    for chunk in chunkGenerator(filename):
+        keyFile.chunkKeys.append(chunk.key)
+        chunks.append(chunk)
+    return (keyfile, chunks)
         
+def dumbChunk(filename):
+    # Creates ONE chunk of all the data
+    raw = ""
+    with open(filename,'r') as fin:
+        raw = fin.read()
+    
+    fileKey =  MultiHash.genHash(filename, 0x12)
+    contentKey = MultiHash.genHash(raw, 0x12)
+    print(fileKey)
+    print(contentKey)
+    
+def textChunkGenerator(filename):
+    pass
+    
+    
+    
+def test():
+    dumbChunk("hamlet.txt")
+
+
+
+    
+if __name__ ==  '__main__':
+    test()
